582f5221953b73d0cd2c3fe4152fd219de9d9ade
Виктор <vitya.tigr15@mail.ru>
2025-12-29 21:48:25 +0300

    Новая ветка51

--- main-service/src/main/java/MainApplication.java
+++ main-service/src/main/java/MainApplication.java
@@ -2,8 +2,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainServiceApplication {
+public class MainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainServiceApplication.class, args);
+        SpringApplication.run(MainApplication.class, args);
     }
 }
--- stats-service/stats-server/src/main/java/server/StatsApplication.java
+++ stats-service/stats-server/src/main/java/server/StatsApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatsServerApplication {
+public class StatsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(StatsServerApplication.class, args);
+        SpringApplication.run(StatsApplication.class, args);
     }
 }
