ed3b9263be1748dcc9f85742dc0eb696b0a344e2
Виктор <vitya.tigr15@mail.ru>
2026-01-05 02:24:25 +0300

    Основная ветка84

--- main-service/src/main/java/ru/practicum/user/service/UserService.java
+++ main-service/src/main/java/ru/practicum/user/service/UserService.java
@@ -1,13 +1,57 @@
 package ru.practicum.user.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.error.exceptions.BadRequestException;
+import ru.practicum.error.exceptions.ConflictException;
+import ru.practicum.error.exceptions.NotFoundException;
 import ru.practicum.user.dto.NewUserRequest;
 import ru.practicum.user.dto.UserDto;
+import ru.practicum.user.mapper.UserMapper;
+import ru.practicum.user.model.User;
+import ru.practicum.user.repository.UserRepository;
+
 import java.util.Collection;
 
-public interface UserService {
-    UserDto addUser(NewUserRequest request);
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
 
-    Collection<UserDto> getAllUsers(Collection<Long> ids, int from, int size);
+    private final UserRepository repository;
+    private final UserMapper userMapper;
 
-    void deleteUser(Long id);
+    public UserDto addUser(NewUserRequest request) {
+        if (request == null) {
+            throw new BadRequestException("Запрос на добавление нового пользователя не может быть null");
+        }
+
+        if (repository.existsByEmail(request.getEmail())) {
+            throw new ConflictException("Пользователь с email: " + request.getEmail() + " существует");
+        }
+
+        User user = repository.save(userMapper.toUser(request));
+        return userMapper.toUserDto(user);
+    }
+
+    public void deleteUser(Long id) {
+        if (!repository.existsById(id)) {
+            throw new NotFoundException("Пользователь с id: " + id + " в базе отсутствует");
+        }
+        repository.deleteById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public Collection<UserDto> getAllUsers(Collection<Long> ids, int from, int size) {
+        Pageable pageable = PageRequest.of(from / size, size);
+
+        if (ids != null && !ids.isEmpty()) {
+            return userMapper.toUserDtoList(repository.findByIdIn(ids, pageable).getContent());
+        } else {
+            return userMapper.toUserDtoList(repository.findAll(pageable).getContent());
+        }
+    }
 }
