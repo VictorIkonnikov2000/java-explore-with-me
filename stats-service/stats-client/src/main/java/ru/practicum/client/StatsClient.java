5b5b0bbd599ad02557a5ad46e391c9d6bfc5a4cf
Виктор <vitya.tigr15@mail.ru>
2026-01-04 23:17:15 +0300

    Основная ветка60

--- stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @Component
 @Slf4j
-public class StatClient {
+public class StatsClient {
 
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
@@ -27,7 +27,7 @@ public class StatClient {
     @Value("${stats-server.url:http://localhost:9090}")
     private String serverUrl;
 
-    public StatClient() {
+    public StatsClient() {
         this.restTemplate = new RestTemplate();
     }
 
