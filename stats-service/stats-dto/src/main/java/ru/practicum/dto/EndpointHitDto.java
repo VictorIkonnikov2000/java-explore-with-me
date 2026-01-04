95bcccda9c47f8cb1f759f2ab9720815783698ce
Виктор <vitya.tigr15@mail.ru>
2026-01-05 00:15:20 +0300

    Основная ветка71

--- stats-service/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -1,25 +1,50 @@
 package ru.practicum.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import static ru.practicum.constants.StandardDateTimeFormats.DATE_TIME_FORMAT;
 
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class EndpointHitDto {
-    @NotBlank(message = "Идентификатор сервиса не может быть пустым")
+    private Long id;
     private String app;
-    @NotBlank(message = "URI не может быть пустым")
     private String uri;
-    @NotBlank(message = "ip пользователя не может быть пустым")
     private String ip;
-    @NotNull(message = "Дата и время запроса к эндпоинту не может быть пустым")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_TIME_FORMAT)
     private LocalDateTime timestamp;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EndpointHitDto that = (EndpointHitDto) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(app, that.app) &&
+                Objects.equals(uri, that.uri) &&
+                Objects.equals(ip, that.ip) &&
+                Objects.equals(timestamp, that.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, app, uri, ip, timestamp);
+    }
+
+    @Override
+    public String toString() {
+        return "EndpointHitDto{" +
+                "id=" + id +
+                ", app='" + app + '\'' +
+                ", uri='" + uri + '\'' +
+                ", ip='" + ip + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
 }
