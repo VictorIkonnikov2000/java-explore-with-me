1e310b66f272e40e5d5aa813b0b56afdb76d68a8
Виктор <vitya.tigr15@mail.ru>
2026-01-05 22:50:22 +0300

    Основная ветка91

--- main-service/src/main/java/ru/practicum/request/mapper/ParticipationRequestMapper.java
+++ main-service/src/main/java/ru/practicum/request/mapper/ParticipationRequestMapper.java
@@ -1,35 +1,22 @@
 package ru.practicum.request.mapper;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.request.dto.ParticipationRequestDto;
 import ru.practicum.request.model.ParticipationRequest;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Component
-public class ParticipationRequestMapper {
+import static ru.practicum.constants.StandardDateTimeFormats.DATE_TIME_FORMAT;
 
-    public ParticipationRequestDto toParticipationRequestDto(ParticipationRequest request) {
-        if (request == null) {
-            return null;
-        }
+@Mapper(componentModel = "spring")
+public interface ParticipationRequestMapper {
 
-        return ParticipationRequestDto.builder()
-                .id(request.getId())
-                .created(request.getCreated()) // Форматирование сделает Jackson через @JsonFormat в DTO
-                .event(request.getEvent() != null ? request.getEvent().getId() : null)
-                .requester(request.getRequester() != null ? request.getRequester().getId() : null)
-                .status(request.getStatus())
-                .build();
-    }
+    @Mapping(target = "created", source = "request.created",
+            dateFormat = DATE_TIME_FORMAT)
+    @Mapping(target = "event", source = "request.event.id")
+    @Mapping(target = "requester", source = "request.requester.id")
+    ParticipationRequestDto mapToParticipationRequestDto(ParticipationRequest request);
 
-    public List<ParticipationRequestDto> toFullDtoList(List<ParticipationRequest> requests) {
-        if (requests == null) {
-            return null;
-        }
-        return requests.stream()
-                .map(this::toParticipationRequestDto)
-                .collect(Collectors.toList());
-    }
+    List<ParticipationRequestDto> toFullDtoList(List<ParticipationRequest> requests);
 }
