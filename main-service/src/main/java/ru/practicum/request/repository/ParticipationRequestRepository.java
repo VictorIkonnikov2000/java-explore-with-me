d2967478d1572eabef0c60ce154a7da6997733f2
Виктор <vitya.tigr15@mail.ru>
2026-01-07 00:31:15 +0300

    Основная ветка122

--- main-service/src/main/java/ru/practicum/request/repository/ParticipationRequestRepository.java
+++ main-service/src/main/java/ru/practicum/request/repository/ParticipationRequestRepository.java
@@ -3,12 +3,9 @@ package ru.practicum.request.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ru.practicum.event.model.Event;
 import ru.practicum.request.model.ParticipationRequest;
 import ru.practicum.request.model.RequestStatus;
-import ru.practicum.user.model.User;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
