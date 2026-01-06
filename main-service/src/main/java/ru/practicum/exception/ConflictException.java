a36bd0e798cb8c3954fc989230475f125126e869
Виктор <vitya.tigr15@mail.ru>
2026-01-06 05:51:09 +0300

    Основная ветка115

--- main-service/src/main/java/ru/practicum/exception/ConflictException.java
+++ main-service/src/main/java/ru/practicum/exception/ConflictException.java
@@ -4,8 +4,4 @@ public class ConflictException extends RuntimeException {
     public ConflictException(String message) {
         super(message);
     }
-
-    public ConflictException(String message, Object... args) {
-        super(String.format(message, args));
-    }
 }
--- main-service/src/main/java/ru/practicum/exception/ForbiddenException.java
+++ main-service/src/main/java/ru/practicum/exception/ForbiddenException.java
@@ -4,8 +4,4 @@ public class ForbiddenException extends RuntimeException {
     public ForbiddenException(String message) {
         super(message);
     }
-
-    public ForbiddenException(String message, Object... args) {
-        super(String.format(message, args));
-    }
 }
